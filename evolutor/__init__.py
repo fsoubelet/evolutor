e5b0cf161f4107fbade711659842730fcb6bc204
Felix Soubelet <felix.soubelet@protonmail.com>
2025-04-25 17:07:37 +0200

    and import export here

--- evolutor/__init__.py
+++ evolutor/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "0.2.0"
 
-from ._evolution import ibs_evolution
+from ._evolution import ibs_and_sr_evolution, ibs_evolution
 from ._record import Records
 
-__all__ = ["ibs_evolution", "Records"]
+__all__ = ["ibs_and_sr_evolution", "ibs_evolution", "Records"]
